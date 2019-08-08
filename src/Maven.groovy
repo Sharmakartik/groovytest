e6c3bbcfa322dc7c47b70a5f6c5338ead91cf3ed
Sharmakartik <Kartik.gs@outlook.com>
2019-08-08 13:10:02 +0530

    Create Maven.groovy

---
+++ src/Maven.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/groovy
+
+class Maven implements Serializable {
+  def steps
+  Shell(steps) {this.steps = steps}
+
+    def mvn(args) {
+    steps.sh "${steps.tool 'jenkins-mvn'}/bin/mvn -o ${args}"
+  }
+}
