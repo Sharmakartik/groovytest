7ec1503d27a1d275ebb702471fea04ec2c5b15e7
Sharmakartik <Kartik.gs@outlook.com>
2019-08-08 13:20:42 +0530

    Update notification.groovy

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -1,1 +1,26 @@
+#!/usr/bin/groovy
 
+def slack(String buildresult)
+{
+  if ( buildResult == "SUCCESS" ) {
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+  }
+  else( buildResult == "FAILURE" ) { 
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+  }
+}
+
+def email(String buildresult)
+{
+  if ( buildResult == "SUCCESS" ) {
+  mail to: 'abhishek.dubey@opstree.com',
+    subject: "Job '${JOB_NAME}' (${BUILD_NUMBER}) was success",
+    body: "Please go to ${BUILD_URL} and verify the build"
+    
+  }
+  else ( buildResult == "FAILURE" ) { 
+    mail to: 'abhishek.dubey@opstree.com',
+    subject: "Job '${JOB_NAME}' (${BUILD_NUMBER}) was failed",
+    body: "Please go to ${BUILD_URL} and verify the build"
+  }
+}
